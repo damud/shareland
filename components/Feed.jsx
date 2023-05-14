d9b29eaa42ea0a6d79e742f2f30c7ede1ef23340
Daniel Mudlaff <dmudlaff89@gmail.com>
2023-05-14 14:40:25 +0200

    Created search box layout

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,5 +1,26 @@
+"use client";
+import { useState, useEffect } from "react";
+import PromptCard from "./PromptCard";
+
 const Feed = () => {
-  return <div></div>;
+  const [serchText, setSearchText] = useState("");
+
+  const handleSearchChange = e => {};
+
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or a username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+    </section>
+  );
 };
 
 export default Feed;
