789261c36f1427fd3e30a03a02fcd5e5396effa3
Daniel Mudlaff <dmudlaff89@gmail.com>
2023-05-17 19:36:01 +0200

    Created deleted edit prompt api

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -37,3 +37,15 @@ export const PATCH = async (request, { params }) => {
     return new Response("Failed to updated prompt", { status: 500 });
   }
 };
+
+export const DELETE = async (request, { params }) => {
+  try {
+    await connectToDB();
+
+    await Prompt.findByIdAndRemove(params.id);
+
+    return new Response("Prompt deleted successfully", { status: 200 });
+  } catch (error) {
+    return new Response("Failed to delete prompt", { status: 500 });
+  }
+};
