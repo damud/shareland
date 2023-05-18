086af89bfada6b18623602a46ecbeeeefef1a7cb
Daniel Mudlaff <dmudlaff89@gmail.com>
2023-05-18 19:22:34 +0200

    Fixed undefined pathname edit and remove prompt button

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -3,15 +3,13 @@
 import { useState } from "react";
 import Image from "next/image";
 import { useSession } from "next-auth/react";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   const { data: session } = useSession();
-  console.log(session?.user.id === post.creator?._id);
-  const { pathName } = usePathname();
-  const router = useRouter();
-
   const [copied, setCopied] = useState("");
+  const pathName = usePathname();
+
   const handleCopy = () => {
     setCopied(post.prompt);
     navigator.clipboard.writeText(post.prompt);
@@ -58,20 +56,22 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
       >
         #{post.tag}
       </p>
-      <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
-        <p
-          className="font-inter text-sm green_gradient cursor-pointer"
-          onClick={handleEdit}
-        >
-          Edit
-        </p>
-        <p
-          className="font-inter text-sm orange_gradient cursor-pointer"
-          onClick={handleDelete}
-        >
-          Delete
-        </p>
-      </div>
+      {session?.user.id === post.creator?._id && pathName === "/profile" && (
+        <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+          <p
+            className="font-inter text-sm green_gradient cursor-pointer"
+            onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p
+            className="font-inter text-sm orange_gradient cursor-pointer"
+            onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   );
 };
