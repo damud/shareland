b618c55c55d0a52098a48c19b840c89e89c8373b
Daniel Mudlaff <dmudlaff89@gmail.com>
2023-05-19 12:20:38 +0200

    Created user's profile page endpoint

---
+++ app/profile/[id]/page.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
+
+import Profile from "@components/Profile";
+
+const userProfile = ({ params }) => {
+  const searchParams = useSearchParams();
+  const userName = searchParams.get("name");
+
+  const [userPosts, setUserPosts] = useState();
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${params?.id}/posts`);
+      const data = await response.json();
+
+      setUserPosts(data);
+    };
+
+    if (params?.id) fetchPosts();
+  }, [params.id]);
+
+  return (
+    <Profile
+      name={userName}
+      description={`Welcome to ${userName}'s personalized profile page. Explore ${userName}'s exceptional prompts and be inspired by the power of their imagination`}
+      data={userPosts}
+    />
+  );
+};
+
+export default userProfile;
